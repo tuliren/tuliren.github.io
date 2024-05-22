cf9a0e36cc1a08b4a11d22a722f6b66d3f01e619
Liren Tu <tuliren@gmail.com>
2024-05-21 20:39:29 -0700

    Add mantine

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@mantine/core/styles.css';
+
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
