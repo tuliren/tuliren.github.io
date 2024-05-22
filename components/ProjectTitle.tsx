986c5e55d8a064fac07374fb5efab45e26525843
Liren Tu <tuliren@gmail.com>
2024-05-21 22:27:05 -0700

    Format files

--- components/ProjectTitle.tsx
+++ components/ProjectTitle.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import {Text, Title} from "@mantine/core";
+import { Text, Title } from '@mantine/core';
+import React, { FC } from 'react';
 
 interface ProjectTitleProps {
   title: string;
@@ -7,10 +7,16 @@ interface ProjectTitleProps {
 
 const ProjectTitle: FC<ProjectTitleProps> = ({ title }) => {
   return (
-    <Text component="span" variant="gradient" fw={800} gradient={{ from: '#0070c9', to: '#e66465', deg: 45 }} inherit>
+    <Text
+      component="span"
+      variant="gradient"
+      fw={800}
+      gradient={{ from: '#0070c9', to: '#e66465', deg: 45 }}
+      inherit
+    >
       {title}
     </Text>
   );
-}
+};
 
 export default ProjectTitle;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
+import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import type { AppProps } from 'next/app';
+import React from 'react';
 
-import type {AppProps} from 'next/app';
-import {MantineProvider} from '@mantine/core';
-import React from "react";
-
-
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
       <Component {...pageProps} />
